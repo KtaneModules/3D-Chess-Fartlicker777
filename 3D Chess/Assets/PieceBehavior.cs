fdece469dbe32017b9a6afa58601099d08fc8aa7
Apolo Simon <62307787+Fartlicker777@users.noreply.github.com>
2023-07-09 05:01:36 -0500

    Main framework

---
+++ 3D Chess/Assets/PieceBehavior.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class PieceBehavior : MonoBehaviour {
+
+   public bool CheckPiece (string Piece, int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) {
+      switch (Piece) {
+         case "Knight":
+            return Knight(FromL, FromR, FromC, ToL, ToR, ToC);
+         case "Bishop":
+            return Bishop(FromL, FromR, FromC, ToL, ToR, ToC);
+         case "Rook":
+            return Rook(FromL, FromR, FromC, ToL, ToR, ToC);
+         case "Queen":
+            return Queen(FromL, FromR, FromC, ToL, ToR, ToC);
+         case "King":
+            return King(FromL, FromR, FromC, ToL, ToR, ToC);
+         default:
+            return false;
+      }
+   }
+
+   public bool Rook (int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) { //For a rook to attack a piece, it needs to share exactly 2 coordinates. So XY, XZ, or YZ
+
+      int Match = 0;
+      if (FromL == ToL) {
+         Match++;
+      }
+      if (FromR == ToR) {
+         Match++;
+      }
+      if (FromC == ToC) {
+         Match++;
+      }
+      if (Match == 2) {
+         return true;
+      }
+      return false;
+   }
+
+   public bool Bishop (int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) { //For a bishop to attack a piece, it needs to share exactly 1 coordinate and the difference between the other two must be the same.
+      if (FromL == ToL) {
+         if (((int) Mathf.Abs(FromR - ToR)) == (int) Mathf.Abs(FromC - ToC)) {
+            return true;
+         }
+         return false;
+      }
+      else if (FromR == ToR) {
+         if (((int) Mathf.Abs(FromC - ToC)) == (int) Mathf.Abs(FromL - ToL)) {
+            return true;
+         }
+         return false;
+      }
+      else if (FromC == ToC) {
+         if (((int) Mathf.Abs(FromR - ToR)) == (int) Mathf.Abs(FromL - ToL)) {
+            return true;
+         }
+         return false;
+      }
+      return false;
+   }
+
+   public bool King (int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) { //For a king to attack a piece, it needs to have at most two off-by-ones and not have a coordinate difference be more than 1.
+      int LDif = Mathf.Abs(FromL - ToL);
+      int RDif = Mathf.Abs(FromR - ToR);
+      int CDif = Mathf.Abs(FromC - ToC);
+      int Dif = LDif + RDif + CDif;
+
+      if (LDif > 1 || RDif > 1 || CDif > 1 || Dif > 2) {
+         return false;
+      }
+      
+      return true;
+   }
+
+   public bool Queen (int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) { //Same as bishop and rook
+      return Rook(FromL, FromR, FromC, ToL, ToR, ToC) || Bishop(FromL, FromR, FromC, ToL, ToR, ToC);
+   }
+
+   public bool Knight (int FromL, int FromR, int FromC, int ToL, int ToR, int ToC) { //Must be 3 tiles off.
+      if (FromL == ToL) {
+         if ((Mathf.Abs(FromR - ToR) == 1 && Mathf.Abs(FromC - ToC) == 2) || (Mathf.Abs(FromC - ToC) == 1 && Mathf.Abs(FromR - ToR) == 2)) {
+            return true;
+         }
+      }
+      else if (Mathf.Abs(FromL - ToL) == 1) {
+         if ((FromR == ToR && Mathf.Abs(FromC - ToC) == 2) || (FromC == ToC && Mathf.Abs(FromR - ToR) == 2)) {
+            return true;
+         }
+      }
+      else if (Mathf.Abs(FromL - ToL) == 2) {
+         if ((FromR == ToR && Mathf.Abs(FromC - ToC) == 1) || (FromC == ToC && Mathf.Abs(FromR - ToR) == 1)) {
+            return true;
+         }
+      }
+      return false;
+   }
+}
---
+++ 3D Chess/Assets/ThreeDimensionalChess.cs
@@ -0,0 +1,283 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+using Math = ExMath;
+
+public class ThreeDimensionalChess : MonoBehaviour {
+
+   public KMBombInfo Bomb;
+   public KMAudio Audio;
+
+   public KMSelectable[] CoordinateButtons;
+   public KMSelectable[] SubmitButtons;
+
+   public TextMesh[] SubmitDisplays;
+   public TextMesh CoordinateDisplay;
+
+   string[][] CoordinateNotation = new string[][] {
+      new string[] { "I", "II", "III", "IV", "V"},
+      new string[] { "A", "B", "C", "D", "E"},
+      new string[] { "1", "2", "3", "4", "5"},
+   };
+   string[] PieceTypes = { "King", "Knight", "Bishop", "Rook", "Queen"};
+   string[] PieceTypesAbbr = { "K", "N", "B", "R", "Q" };
+   int[] SubmitCoordinateIndices = { 0, 0, 0, 0};
+
+   int[][][] Board = new int[][][] {
+      new int[][] {
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+      },
+
+      new int[][] {
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+      },
+
+      new int[][] {
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+      },
+
+      new int[][] {
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+      },
+
+      new int[][] {
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+         new int[] { 0, 0, 0, 0, 0},
+      }
+   };
+
+   int[][] Positions = new int[][] {
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+      new int[] { 0, 0, 0},
+   };
+
+   Piece[] Pieces = {
+      new Piece(), new Piece(), new Piece(), new Piece(), new Piece(), new Piece(), new Piece()
+   };
+
+   string FinalPieceType = "";
+
+   Piece Eighth = new Piece();
+   public PieceBehavior PB;
+
+   int Offset = 0;
+
+   int ULTRATIMWI = 0;
+
+   static int ModuleIdCounter = 1;
+   int ModuleId;
+   private bool ModuleSolved;
+
+   void Awake () {
+      ModuleId = ModuleIdCounter++;
+      GetComponent<KMBombModule>().OnActivate += Activate;
+      
+      foreach (KMSelectable Button in CoordinateButtons) {
+          Button.OnInteract += delegate () { CButPress(Button); return false; };
+      }
+
+      foreach (KMSelectable Button in SubmitButtons) {
+         Button.OnInteract += delegate () { SButPress(Button); return false; };
+      }
+
+   }
+
+   void CButPress (KMSelectable C) {
+      for (int i = 0; i < 7; i++) {
+         if (C == CoordinateButtons[i]) {
+            int temp = i - Offset;
+            if (temp < 0) {
+               temp += 7;
+            }
+            UpdateCoordinateDisplay(temp);
+         }
+      }
+   }
+
+   void SButPress (KMSelectable S) {
+      for (int i = 0; i < 5; i++) {
+         if (S == SubmitButtons[i]) {
+            if (i == 4) {
+               Piece Test = new Piece(SubmitCoordinateIndices[0], SubmitCoordinateIndices[1], SubmitCoordinateIndices[2], PieceTypes[SubmitCoordinateIndices[0]]);
+               if (PB.CheckPiece(Pieces[6].T, Pieces[6].L, Pieces[6].R, Pieces[6].C, Test.L, Test.R, Test.C) && PB.CheckPiece(Test.T, Test.L, Test.R, Test.C, Pieces[0].L, Pieces[0].R, Pieces[0].C)) {
+                  Solve();
+               }
+               else {
+                  Strike();
+               }
+            }
+            else {
+               SubmitCoordinateIndices[i] = (SubmitCoordinateIndices[i] + 1) % 5;
+               UpdateSubmitDisplay();
+            }
+         }
+      }
+   }
+
+   void UpdateCoordinateDisplay (int i) {
+      CoordinateDisplay.text = PieceTypesAbbr[Array.IndexOf(PieceTypes, Pieces[i].T)] + CoordinateNotation[0][Pieces[i].L] + CoordinateNotation[1][Pieces[i].C] + CoordinateNotation[2][Pieces[i].R];
+   }
+
+   void UpdateSubmitDisplay () {
+      SubmitDisplays[0].text = PieceTypesAbbr[SubmitCoordinateIndices[0]];
+      for (int i = 0; i < 3; i++) {
+         SubmitDisplays[i + 1].text = CoordinateNotation[i][SubmitCoordinateIndices[i + 1]];
+      }
+   }
+
+   void OnDestroy () { //Shit you need to do when the bomb ends
+      
+   }
+
+   void Activate () { //Shit that should happen when the bomb arrives (factory)/Lights turn on
+
+   }
+
+   void Start () { //Shit
+      Offset = Rnd.Range(0, 7);
+
+      SubmitDisplays[0].text = "K";
+      for (int i = 0; i < 3; i++) {
+         SubmitDisplays[i + 1].text = CoordinateNotation[i][0];
+      }
+      //This resets all the submit displays to their default state.
+
+      GeneratePuzzle();
+      CoordinateDisplay.text = "";
+   }
+
+   void LogPieces () {
+      //Debug.Log(Offset);
+      for (int i = 0; i < 7; i++) {
+         Debug.LogFormat("[3D Chess #{0}] Piece {1} is a {2} at {3}{4}{5}. This is displayed on button {6}.", ModuleId, i + 1, Pieces[i].T, CoordinateNotation[0][Pieces[i].L], CoordinateNotation[1][Pieces[i].C], CoordinateNotation[2][Pieces[i].R], (Offset + i) % 7 + 1);
+      }
+
+      Debug.LogFormat("[3D Chess #{0}] The final piece is a {1} at {2}{3}{4}.", ModuleId, Eighth.T, CoordinateNotation[0][Eighth.L], CoordinateNotation[1][Eighth.C], CoordinateNotation[2][Eighth.R]);
+   }
+
+   void GeneratePuzzle () {
+      int Timwi = 0;
+      ULTRATIMWI++;
+      if (ULTRATIMWI > 10) {
+         CoordinateDisplay.text = "ERROR";
+         Solve();
+      }
+
+      Pieces[0].ChangePiece(Rnd.Range(0, 5), Rnd.Range(0, 5), Rnd.Range(0, 5), PieceTypes[Rnd.Range(0, 5)]);   //Gets our starting piece, can be anything we want
+
+      for (int i = 1; i < 7; i++) {
+         RestartPiece:
+         Timwi++;
+         if (Timwi == 100000) {  //If it is impossible to place the next piece, we just restart the puzzle.
+            GeneratePuzzle();
+            return;
+         }
+         do {
+            Pieces[i].ChangePiece(Rnd.Range(0, 5), Rnd.Range(0, 5), Rnd.Range(0, 5), PieceTypes[Rnd.Range(0, 5)]);
+         } while (!PB.CheckPiece(Pieces[i - 1].T, Pieces[i - 1].L, Pieces[i - 1].R, Pieces[i - 1].C, Pieces[i].L, Pieces[i].R, Pieces[i].C)); //Check that the previous piece attacks the current one
+         
+
+         for (int j = 0; j < i - 1; j++) {
+            if (PB.CheckPiece(Pieces[j].T, Pieces[j].L, Pieces[j].R, Pieces[j].C, Pieces[i].L, Pieces[i].R, Pieces[i].C)) { //Checks if any previous piece blocks the current one, although this is done shittily so it prolly has side effects that I don't care enough to fix.
+               goto RestartPiece;
+            }
+         }
+         Timwi = 0;
+      }
+
+      FinalPieceType =  PieceTypes[(Array.IndexOf(PieceTypes, Pieces[0].T) + Array.IndexOf(PieceTypes, Pieces[0].T)) % 5]; //Follow the table from the manual.
+
+      for (int i = 0; i < 100000; i++) {
+         do {
+            Eighth.ChangePiece(Rnd.Range(0, 5), Rnd.Range(0, 5), Rnd.Range(0, 5), PieceTypes[Rnd.Range(0, 5)]);         //Generates the last piece such that it attacks the first and is attacked by the last. Probably.
+         } while (Eighth.T != FinalPieceType);
+         
+         if (PB.CheckPiece(Eighth.T, Eighth.L, Eighth.R, Eighth.C, Pieces[0].L, Pieces[0].R, Pieces[0].C) && PB.CheckPiece(Pieces[6].T, Pieces[6].L, Pieces[6].R, Pieces[6].C, Eighth.L, Eighth.R, Eighth.C)) {
+            break;
+         }
+      }
+
+      LogPieces();
+   }
+
+   void Solve () {
+      GetComponent<KMBombModule>().HandlePass();
+   }
+
+   void Strike () {
+      GetComponent<KMBombModule>().HandleStrike();
+   }
+
+#pragma warning disable 414
+   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+   IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+   }
+
+   IEnumerator TwitchHandleForcedSolve () {
+      yield return null;
+   }
+
+   public class Piece {
+
+      public int L = 0;
+      public int C = 0;
+      public int R = 0;
+
+      public string T = "Pawn";
+
+      public Piece () {
+         L = 0;
+         C = 0;
+         R = 0;
+         T = "Pawn";
+      }
+
+      public Piece (int Layer, int Column, int Row, string PType) {  //I fucking hate making constructors.
+         L = Layer;
+         C = Column;
+         R = Row;
+         T = PType;
+      }
+
+      public void ChangePiece (int Layer, int Column, int Row, string PType) {
+         L = Layer;
+         C = Column;
+         R = Row;
+         T = PType;
+      }
+   }
+
+}
+
